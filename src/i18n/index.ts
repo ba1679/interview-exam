e26363ada6e88e532ecb5c682195ddc9a75c7012
hsin hui <shinhuei1@gmail.com>
2022-08-25 22:07:18 +0800

    chore: setup browser detect i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import detector from 'i18next-browser-languagedetector';
+import backend from 'i18next-xhr-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(detector)
+  .use(backend)
+  .use(initReactI18next).init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['navigator', 'cookie'],
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
