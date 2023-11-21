2f2c31502518423139d40e62e22fbb94a9d80c0c
Render Lai <render.lai@alfred.camera>
2023-11-21 20:50:18 +0800

    chore: move firebase config to repo secrects

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,13 +12,15 @@ import { getStorage } from 'firebase/storage';
 import { TloginForm, TsignUpForm } from 'types';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCiT0kNfiMR8-7Jx42g9UEQNO68Has-EQ4',
-  authDomain: 'alfred-test-4082b.firebaseapp.com',
-  projectId: 'alfred-test-4082b',
-  storageBucket: 'alfred-test-4082b.appspot.com',
-  messagingSenderId: '576696861946',
-  appId: '1:576696861946:web:eca0ae9becb2a818643543',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+console.log('log: ', process.env, process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
@@ -26,7 +28,10 @@ export const auth = getAuth(app);
 export const signUpAndUpdateUserInfo = async (data: TsignUpForm) => {
   const { email, password, firstName, lastName, photoURL } = data;
   const { user } = await createUserWithEmailAndPassword(auth, email, password);
-  await updateProfile(user, { displayName: `${lastName} ${firstName}`, photoURL });
+  await updateProfile(user, {
+    displayName: `${lastName} ${firstName}`,
+    photoURL,
+  });
   return user;
 };
 
